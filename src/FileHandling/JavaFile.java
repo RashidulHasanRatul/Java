1ba6f11ecd108502b1a007b26f2e2116ff11edec
Rashidul Hasan <rhratul12@gmail.com>
2021-10-24 00:28:03 +0600

    FileHandling

---
+++ src/FileHandling/JavaFile.java
@@ -0,0 +1,20 @@
+package FileHandling;
+import java.io.File;  // Import the File class
+public class JavaFile {
+    /*canRead()	Boolean	Tests whether the file is readable or not
+canWrite()	Boolean	Tests whether the file is writable or not
+createNewFile()	Boolean	Creates an empty file
+delete()	Boolean	Deletes a file
+exists()	Boolean	Tests whether the file exists
+getName()	String	Returns the name of the file
+getAbsolutePath()	String	Returns the absolute pathname of the file
+length()	Long	Returns the size of the file in bytes
+list()	String[]	Returns an array of the files in the directory
+mkdir()	Boolean	Creates a directory
+    * */
+
+  // Specify the filename
+
+
+    File myObj = new File("filename.txt"); // Specify the filename
+}
